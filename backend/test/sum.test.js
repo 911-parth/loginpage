7a812b72df84623fd7b074e028309aa90968df56
Parth Chauhan <parthchauhan99248@gmail.com>
2026-02-18 18:57:28 +0100

    test6

--- backend/test/sum.test.js
+++ backend/test/sum.test.js
@@ -3,9 +3,9 @@ const sum = require("../sum");
 test("2 + 2 should equal 4", () => {
   expect(sum(2, 2)).toBe(4);
 });
-test("3 + 1 should equal 4", () => {
-  expect(sum(3, 1)).toBe(4);
+test("2 + 2 should equal 4", () => {
+  expect(sum(1, 2)).toBe(3);
 });
-test("3 + 1 should equal 4", () => {
-  expect(sum(2, 0)).toBe(4);
+test("2 + 2 should equal 4", () => {
+  expect(sum(2, 3)).toBe(5);
 });
